0586d3227f3389cb9056f1f9fc815b09ff98f474
lamch0 <gordonlamch@gmail.com>
2022-05-07 23:41:02 +0800

    Create pages.js

---
+++ routes/pages.js
@@ -0,0 +1,4 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://stu142:p215347-@csci2720.m2qbq.mongodb.net/stu142')
