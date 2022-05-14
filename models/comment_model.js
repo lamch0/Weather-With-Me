82a99a43d0249d192ecc0872a9e2814a9ea37d55
cychoi0 <79830983+cychoi0@users.noreply.github.com>
2022-05-14 22:10:04 +0800

    Update comment_model.js

--- models/comment_model.js
+++ models/comment_model.js
@@ -1,3 +1,9 @@
+// 1155143373 Lam Lok Hin 
+// 1155143281 Choi Chung Yu 
+// 1155142376 Cheung King Wa 
+// 1155110159 Cheung Hing Wing 
+// 1155142672 Kwok Chun Yin
+// 1155143825 Lam Cheuk Hin
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 // const location = require('./location_model');
