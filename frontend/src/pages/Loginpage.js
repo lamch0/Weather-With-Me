dbeea9eda3f8e666369e06f02305a0c7c0ca94e6
cheungkw0 <1155142376@link.cuhk.edu.hk>
2022-05-14 21:48:12 +0800

    comments

--- frontend/src/pages/Loginpage.js
+++ frontend/src/pages/Loginpage.js
@@ -1,9 +1,9 @@
-// 1155143373 Lam Lok Hin (lamlokhin)
-// 1155143281 Choi Chung Yu (cychoi0)
-// 1155142376 Cheung King Wa (cheungkw0)
-// 1155110159 Cheung Hing Wing (hingwingc)
-// 1155142672 Kwok Chun Yin(EdmondKwok)
-// 1155143825 Lam Cheuk Hin(lamch0)
+// 1155143373 Lam Lok Hin 
+// 1155143281 Choi Chung Yu 
+// 1155142376 Cheung King Wa 
+// 1155110159 Cheung Hing Wing 
+// 1155142672 Kwok Chun Yin
+// 1155143825 Lam Cheuk Hin
 
 
 import "./Loginpage.css"
